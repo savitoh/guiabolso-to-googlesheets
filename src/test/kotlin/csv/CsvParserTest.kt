0c716a121c898930fb5562650feef62e8a0a5be3
Sávio Raires de Souza <savioraires10@gmail.com>
2022-12-31 01:42:34 -0300

    test: add test for parse CSV (#3)

--- src/test/kotlin/csv/CsvParserTest.kt
+++ src/test/kotlin/csv/CsvParserTest.kt
@@ -1,6 +1,8 @@
 package csv
 
 import csv.CsvParser.CsvWrapper
+import io.kotest.inspectors.forExactly
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
 import java.io.File
@@ -39,6 +41,30 @@ class CsvParserTest {
         messageExpected = "Row: \"[Banco Itaú Unibanco S.A., Banco Itaú Unibanco S.A., 2018-10-08, INT PAG TIT BANCO 422, -164.0, Pagamento de cartão, Extra Column, ]\" doesn't have: 7 columns"
     )
 
+    @Test
+    fun `when csv has expected format then return parsed CSV`() {
+        val path = "/csv/sample_guiabolso.csv"
+
+        getFromResources(path)?.let {
+            val result: Result<List<CsvWrapper>> = csvParser.parse(it)
+
+
+            result.isSuccess shouldBe true
+            val csvWrappers = result.getOrDefault(listOf())
+            csvWrappers shouldHaveSize 7
+            csvWrappers.forExactly(1) { csvWrapper ->
+                csvWrapper shouldBe CsvWrapper(
+                    banco = "Caixa Econômica Federal",
+                    conta = "***4 / **2 / *****425-6",
+                    transactionAt = "2018-12-09",
+                    transactionDescription = "REM BASICA",
+                    transactionValue = "0.0",
+                    category = "Juros"
+                )
+            }
+        }
+    }
+
     private fun testCase(path: String, messageExpected: String) {
         getFromResources(path)?.let {
 
@@ -46,7 +72,7 @@ class CsvParserTest {
 
 
             result.isFailure shouldBe true
-            messageExpected shouldBe result.exceptionOrNull()?.message
+            result.exceptionOrNull()?.message shouldBe messageExpected
         } ?: fail("Cannot open the file: $path")
     }
 
